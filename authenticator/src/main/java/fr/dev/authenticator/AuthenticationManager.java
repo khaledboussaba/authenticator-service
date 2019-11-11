697c3c4493040a215489fb7cf043d3f6d074f3dc
khaled <khaledboussaba@gmail.com>
2019-11-11 14:40:12 +0100

    AutheticationManager created whithout implemenetations of its methdos
    Adding of log4j

---
+++ authenticator/src/main/java/fr/dev/authenticator/AuthenticationManager.java
@@ -0,0 +1,24 @@
+package fr.dev.authenticator;
+
+public class AuthenticationManager {
+	
+	private static String createSalt() {
+		// TODO
+		return null;
+	}
+
+	public static void initializeCredentials(String clearPassword, Credentials credentials) {
+		// TODO
+	}
+	
+	private static String generateHash(String source) {
+		// TODO
+		return null;
+	}
+	
+	public static boolean authenticate(String clearPassword, Credentials credentials) {
+		// TODO
+		return false;
+	}
+	
+}
